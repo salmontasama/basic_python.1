cb41215cdfe644e1a47e1f292a5dd79ff509f426
salmontasama <salmontasama1@gmail.com>
2022-03-09 17:04:13 +0200

    Answers basic

--- while_basic.1.py
+++ while_basic.1.py
@@ -1,6 +1,29 @@
 
+##1##
+num = int(input("enter number"))
+print(num**2)
+##2##
+age = int(input("enter your age"))
+print(age+20)
+##3##
+price  = int(input("enter price"))
+print(price*1.17)
+##4##
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+print(num1+num2,"\n",num1*num2,"\n",num1-num2,"\n",num1//num2)
+##5##
+import time
 
-num = 0
-while num < 10:
-    num +=1
-    print(num)
+localtime = time.asctime(time.localtime(time.time()))
+print("welcome :",'\n', localtime)
+##6##
+num1 = int(input("enter number1"))
+num2 = int(input("enter number2"))
+num3 = int(input("enter number3"))
+num4 = int(input("enter number4"))
+print(float((num1+num2+num3+num4)/4))
+##7##
+nails = int(input("number of nails"))
+print(nails/40)
+
